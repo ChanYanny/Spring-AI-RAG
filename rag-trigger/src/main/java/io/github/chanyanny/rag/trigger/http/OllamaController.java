9cca71f380048325f27b174187e3747dc06d7fb1
ChanYanny <2930066235@qq.com>
2026-01-03 20:59:49 +0800

    feat: ollama ui设计

--- rag-trigger/src/main/java/io/github/chanyanny/rag/trigger/http/OllamaController.java
+++ rag-trigger/src/main/java/io/github/chanyanny/rag/trigger/http/OllamaController.java
@@ -6,13 +6,11 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import reactor.core.publisher.Flux;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ollama")
 public class OllamaController implements IAIService {
